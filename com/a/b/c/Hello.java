846cee8cac14868820bb8bf03015302f52d6fdea
jongbini <77082833+jongbini@users.noreply.github.com>
2021-05-25 17:50:39 +0900

    add hello2

--- com/a/b/c/Hello.java
+++ com/a/b/c/Hello.java
@@ -3,5 +3,6 @@ package com.a.b.c.
   public static void main(Stirng[] args){
     public class Hello(){
       System.out.println("hello world!");
+      System.out.println("hello world2!");
     }
 }
